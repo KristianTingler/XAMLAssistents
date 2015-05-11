d7d91bb504e9f787eebc75f4127bc1f7e14bd5de
KristianTingler <Kristian.Tingler@outlook.com>
2015-05-11 15:33:27 +0200

    Revert "Show me a hover!"
    This reverts commit 25217187957ab0bfa7ef56c0487b2f0cf1621295.

--- ColorConverter/HoverTester/MainWindow.xaml.vb
+++
@@ -1,7 +0,0 @@
-﻿Class MainWindow
-
-    Private Sub Border_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs)
-        MessageBox.Show("Button pressed", String.Empty, MessageBoxButton.OK, MessageBoxImage.Information)
-    End Sub
-
-End Class
